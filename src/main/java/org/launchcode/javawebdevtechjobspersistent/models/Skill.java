4a84de6dcc28219b0c4304d124410512ee52de80
Heather Hecky <hhecky@gmail.com>
2020-10-26 23:28:43 -0500

    finished project thru step 1, working on step 2 and my EmployerController and SkillController are both hung up on the Optional opt = repository,findById() line.....says expect string but getting int, but the variable is defined as an int?

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,5 +1,14 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.Max;
+
+@Entity
 public class Skill extends AbstractEntity {
+
+    @Max(365)
+    private String description;
+
+    public Skill(){};
 
 }
