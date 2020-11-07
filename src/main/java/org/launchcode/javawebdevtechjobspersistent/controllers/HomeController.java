443c7cacad80022a7a36372f38088a34186d3e68
Heather Hecky <hhecky@gmail.com>
2020-11-07 08:15:18 -0600

    committing before trying to do last part of step 3, updating homecontroller

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/controllers/HomeController.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/controllers/HomeController.java
@@ -1,6 +1,10 @@
 package org.launchcode.javawebdevtechjobspersistent.controllers;
 
+import org.launchcode.javawebdevtechjobspersistent.models.Employer;
 import org.launchcode.javawebdevtechjobspersistent.models.Job;
+import org.launchcode.javawebdevtechjobspersistent.models.data.EmployerRepository;
+import org.launchcode.javawebdevtechjobspersistent.models.data.JobRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -8,12 +12,16 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Created by LaunchCode
  */
 @Controller
 public class HomeController {
+
+    @Autowired
+    private EmployerRepository employerRepository;
 
     @RequestMapping("")
     public String index(Model model) {
@@ -26,19 +34,33 @@ public class HomeController {
     @GetMapping("add")
     public String displayAddJobForm(Model model) {
         model.addAttribute("title", "Add Job");
+        model.addAttribute("employers", employerRepository.findAll());
         model.addAttribute(new Job());
         return "add";
     }
 
     @PostMapping("add")
     public String processAddJobForm(@ModelAttribute @Valid Job newJob,
-                                       Errors errors, Model model, @RequestParam int employerId, @RequestParam List<Integer> skills) {
+                                    Errors errors, Model model, @RequestParam int employerId) {  // removed @RequestParam List<Integer> skills
 
         if (errors.hasErrors()) {
             model.addAttribute("title", "Add Job");
             return "add";
         }
 
+         //how do I get employer from drop down selection? , got by ID OR DID I????, how to get name into db?
+       // model.addAttribute("employerId", employerRepository.findById(employerId));
+        //model.addAttribute("employer", employer.name);
+//        Optional optEmployer = employerRepository.findById(employerId);
+//        Employer employer = (Employer) optEmployer.get();
+//        model.addAttribute("employer", employer);
+//        jobRepository.save(newJob);
+        //return "redirect:view/" + newJob.getId();
+
+        model.addAttribute("employer", employerRepository.findById(employerId));
+        model.addAttribute("employerId", newJob.getId());
+        model.addAttribute("employerName", newJob.getName());
+        jobRepository.save(newJob);
         return "redirect:";
     }
 
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,13 +1,21 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotBlank;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Employer extends AbstractEntity {
 
     @NotBlank(message = "Location is required")
     private String location;
+
+    @OneToMany //removed (mappedBy = "employer") as threw error, can't use it w/@JoinColumn
+    @JoinColumn(name = "employer_id") //is this correct? hibernate annotations
+    private final List<Job> jobs = new ArrayList<>();
 
     public Employer(){};
 
@@ -17,5 +25,9 @@ public class Employer extends AbstractEntity {
 
     public void setLocation(String location) {
         this.location = location;
+    }
+
+    public List<Job> getJobs() {
+        return jobs;
     }
 }
